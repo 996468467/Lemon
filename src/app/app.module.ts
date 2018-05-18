3a1a35055970b355a1eb71d0389b382528b4b110
996468467 <996468467@qq.com>
2018-05-18 16:19:51 +0800

    2018-05-18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { CenterPage } from '../pages/center/center';
+import { AttentionPage } from '../pages/attention/attention';
 import { HomePage } from '../pages/home/home';
+import { MessagePage } from '../pages/message/message';
+import { UserPage } from '../pages/user/user';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +16,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    CenterPage,
+    AttentionPage,
     HomePage,
+    MessagePage,
+    UserPage,
     TabsPage
   ],
   imports: [
@@ -26,9 +30,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    CenterPage,
+    AttentionPage,
     HomePage,
+    MessagePage,
+    UserPage,
     TabsPage
   ],
   providers: [
